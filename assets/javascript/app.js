d31895278f8a40319dc6e00a8d444be51de3eed8
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-21 19:08:28 -0500

    Made quite a few changes since last push. Moved the answers radio form into a JSON object and appended that instead. This will allow me to change the text easier whenever the next question is shown, by selecting the answer and using the .text method. Also added a submit answer button. Looking into adding a timer now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,18 +19,25 @@ again */
 
 var correctAnswers;
 var incorrectAnswers;
+var answers = [
+    answer1 = "<div class='form-check'> <input class='form-check-input' type='radio' name='exampleRadios' value='option1'><label class='form-check-label' for='exampleRadios1' id='answer1'>Answer 1</label></div>",
+    answer2 = "<div class='form-check'> <input class='form-check-input' type='radio' name='exampleRadios' value='option2'><label class='form-check-label' for='exampleRadios2' id='answer2'>Answer 2</label></div>",
+    answer3 = "<div class='form-check'> <input class='form-check-input' type='radio' name='exampleRadios' value='option3'><label class='form-check-label' for='exampleRadios2' id='answer3'>Answer 3</label></div>",
+    answer4 = "<div class='form-check'> <input class='form-check-input' type='radio' name='exampleRadios' value='option4'><label class='form-check-label' for='exampleRadios2' id='answer4'>Answer 4</label></div>", 
+    ];
 
 $(".start").click(function firstQuestion() { 
     var firstQuestion = "<div id='firstQuestion'>";
     var row = "<div class='row'>";
     var col1 = "<div class='col-12'>";
     var title1 = "<h1>Question 1</h1>";
-    var question1 = "<p>Test test test</p>";
-    var questionForm1 = "<div class='form-check'> <input class='form-check-input' type='radio' name='exampleRadios' id='exampleRadios1' value='option1' checked> <label class='form-check-label' for='exampleRadios1'>Answer 1</label>";
+    var question1 = "<p>What is Gollum's hobbit name?</p>";
     $("#mainMenu").css("display", "none");
-    $(".container").append(firstQuestion, row, col1, title1, question1, questionForm1);
-
-
-
+    $(".container").append(firstQuestion, row, col1, title1, question1, answers);
+    $("#answer1").text("Smeagol");
+    $("#answer2").text("Merry");
+    $("#answer3").text("Bilbo");
+    $("#answer4").text("Deagol");
+    $(".container").append($("<button type='button' class='continue btn btn-success btn-lg btn-block'>Submit</button>"));
 
 });
